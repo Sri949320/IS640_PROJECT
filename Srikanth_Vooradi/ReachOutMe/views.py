966f710980ad5c58b2abb0bc618f43fe360f8366
Sri949320 <srikanthvooradi9462@gmail.com>
2023-12-11 13:47:50 -0800

    edit 1

--- Srikanth_Vooradi/ReachOutMe/views.py
+++ Srikanth_Vooradi/ReachOutMe/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Contact
+from .forms import ContactForm
 
-# Create your views here.
+def contact_list(request):
+    contacts = Contact.objects.all()
+    return render(request, 'contact_list.html', {'contacts': contacts})
+
+def contact_create(request):
+    form = ContactForm(request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        form.save()
+        return redirect('contact_list')
+    return render(request, 'contact_form.html', {'form': form})
+
+def contact_detail(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    return render(request, 'contact_detail.html', {'contact': contact})
+
+def contact_update(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    form = ContactForm(request.POST or None, instance=contact)
+    if request.method == 'POST' and form.is_valid():
+        form.save()
+        return redirect('contact_list')
+    return render(request, 'contact_form.html', {'form': form, 'contact': contact})
+
+def contact_delete(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    if request.method == 'POST':
+        contact.delete()
+        return redirect('contact_list')
+    return render(request, 'contact_confirm_delete.html', {'contact': contact})
